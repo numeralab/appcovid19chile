8c614c03ee7f2a8374120daeaed4cf1bbdc80acf
Alejandro Dinamarca <63689583+adinamarca@users.noreply.github.com>
2020-07-05 01:10:35 -0400

    Updates

---
+++ precache-manifest.725f0a463b6f2728dbf4df439edb8a74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0288d5e1e5a99ecdb98bb8e991c7f9c8",
+    "url": "/numeralab.github.io/index.html"
+  },
+  {
+    "revision": "552799cac9da97ab2c5c",
+    "url": "/numeralab.github.io/static/css/main.b6482e1f.chunk.css"
+  },
+  {
+    "revision": "cab088da9604c9da2a03",
+    "url": "/numeralab.github.io/static/js/2.1d782926.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/numeralab.github.io/static/js/2.1d782926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "552799cac9da97ab2c5c",
+    "url": "/numeralab.github.io/static/js/main.aef324f3.chunk.js"
+  },
+  {
+    "revision": "f9b7650930ca7a9d7bcf",
+    "url": "/numeralab.github.io/static/js/runtime-main.5c6faacc.js"
+  },
+  {
+    "revision": "db739906a41dba7e98c0fd76584a690a",
+    "url": "/numeralab.github.io/static/media/favicon.db739906.png"
+  },
+  {
+    "revision": "cd3f9caae4c9f43ff152ca9609070a5d",
+    "url": "/numeralab.github.io/static/media/perfiloptimizada.cd3f9caa.jpg"
+  }
+]);
