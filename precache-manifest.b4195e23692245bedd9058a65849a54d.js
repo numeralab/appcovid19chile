bc094e983bd103c85112b67940ec6ecb74ea619e
Alejandro Dinamarca <63689583+adinamarca@users.noreply.github.com>
2020-07-15 20:43:33 -0400

    Updates

---
+++ precache-manifest.b4195e23692245bedd9058a65849a54d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d71855d377d68da47c1145abdbd5806",
+    "url": "/appcovid19chile/index.html"
+  },
+  {
+    "revision": "6b18bb7ea2a7f97ba319",
+    "url": "/appcovid19chile/static/css/main.76b23e25.chunk.css"
+  },
+  {
+    "revision": "f3e3c53a4ba7e05d1f48",
+    "url": "/appcovid19chile/static/js/2.7af78842.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/appcovid19chile/static/js/2.7af78842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b18bb7ea2a7f97ba319",
+    "url": "/appcovid19chile/static/js/main.e6733950.chunk.js"
+  },
+  {
+    "revision": "a3557e517c44d843d352",
+    "url": "/appcovid19chile/static/js/runtime-main.2023ad73.js"
+  },
+  {
+    "revision": "db739906a41dba7e98c0fd76584a690a",
+    "url": "/appcovid19chile/static/media/favicon.db739906.png"
+  },
+  {
+    "revision": "cd3f9caae4c9f43ff152ca9609070a5d",
+    "url": "/appcovid19chile/static/media/perfiloptimizada.cd3f9caa.jpg"
+  }
+]);
